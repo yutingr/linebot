116bb0d938db1c0972c3562d81e0e1f72d1f2151
yutingr <43690980+yutingr@users.noreply.github.com>
2018-12-05 16:23:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from linebot import (
 from linebot.exceptions import (
         InvalidSignatureError
 )
-from linebot.models import (
-        MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
