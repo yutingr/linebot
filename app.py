40ec248e513fd27cc6c56690b6f4624990903c78
yutingr <43690980+yutingr@users.noreply.github.com>
2018-12-05 15:35:08 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = Reply(text=event.message.text)
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 	
 def Reply(text):
